0140f094640a36310ea640b16a8cd29d3cd24813
Nikhil-evol <nikhil95124@gmail.com>
2026-07-09 13:49:04 +0530

    updated

--- back/index.js
+++
@@ -1,9 +0,0 @@
-import dotenv from "dotenv";
-import app from "./src/app.js";
-import { connectDB } from "./src/lib/db.js";
-
-dotenv.config();
-
-connectDB();
-
-export default app;
--- front/src/lib/axios.js
+++ front/src/lib/axios.js
@@ -1,8 +1,6 @@
 import axios from "axios";
 
-const backendUrl = import.meta.env.VITE_API_URL || (import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api");
-
 export const axiosInstance = axios.create({
-  baseURL: backendUrl,
+  baseURL: import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api",
   withCredentials: true,
 });
